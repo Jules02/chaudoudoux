5c36db7d273d75d3736a8ee9629a7776927c8e53
Jules02 <julesdupont02@gmail.com>
2020-04-21 00:45:15 +0200

    Improved the new chaudoudoux form

--- src/Form/ChaudoudouxType.php
+++ src/Form/ChaudoudouxType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Chaudoudoux;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,9 +15,33 @@ class ChaudoudouxType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('image', TextType::class)
-            ->add('toUser', TextType::class)
-            ->add('text', TextType::class)
+            ->add('toUser', TextType::class, [
+                'label'  => false,
+                'attr' => array(
+                    'class' => 'toUser-field',
+                    'placeholder' => "Choisissez un élève, enseignant, personnel de l'administration, du lycée, etc."
+                ),
+            ])
+            ->add('image', ChoiceType::class, [
+                'label'  => false,
+                'attr' => array(
+                    'class' => 'image-field'
+                ),
+                'choices'  => [
+                    'Photo Star wars' => "star-wars.jpg",
+                    'Photo lycée' => "lycee.jpg",
+                ],
+                'expanded' => true,
+                'multiple' => false
+            ])
+            ->add('text', TextareaType::class, [
+                'label'  => false,
+                'attr' => array(
+                    'class' => 'text-field',
+                    'placeholder' => "Soyez créatifs ;)",
+                    'rows' => '10'
+                ),
+            ])
         ;
     }
 
